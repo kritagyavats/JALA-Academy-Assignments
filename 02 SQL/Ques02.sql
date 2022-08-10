ae630862bf4a00e62042c92b1f0405906e2cffed
Kritagya Vats <85698561+kritagyavats@users.noreply.github.com>
2022-08-10 22:59:41 +0530

    Create Ques02.sql

---
+++ 02 SQL/Ques02.sql
@@ -0,0 +1,25 @@
+/* Ques 02) Display all snum without duplicates from all orders.*/
+
+CREATE TABLE Orders (
+ Onum INT NOT NULL,
+    AMT FLOAT NOT NULL,
+    Odate DATE NOT NULL,
+    Cnum INT NOT NULL,
+    Snum INT NOT NULL,
+    CONSTRAINT PK_Orders_Onum PRIMARY KEY(Onum),
+    CONSTRAINT FK_Orders_Cnum FOREIGN KEY(Cnum) REFERENCES Customers (Cnum),
+    CONSTRAINT FK_Orders_Snum FOREIGN KEY(Snum) REFERENCES Salespeople (Snum)
+);
+INSERT INTO Orders VALUES
+    (3001,18.69,'03-OCT-94',2004,1007),
+    (3003,767.19,'03-OCT-94',2001,1001),
+    (3002,1900.10,'03-OCT-94',2007,1004),
+    (3005,5160.45,'03-OCT-94',2003,1002),
+    (3006,1098.16,'04-OCT-94',2004,1007),
+    (3009,1713.23,'04-OCT-94',2002,1003),
+    (3007,75.75,'05-OCT-94',2004,1002),
+    (3008,4723.00,'05-OCT-94',2006,1001),
+    (30010,1309.95,'06-OCT-94',2004,1002),
+    (30011,9891.88,'06-OCT-94',2006,1001).
+SELECT DISTINCT Snum
+FROM Orders.
